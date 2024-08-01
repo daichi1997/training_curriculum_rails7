657745f943e6085ce2f2a0f59e999d4162b569ab
daichi1997 <tagaaan@gmail.com>
2024-08-01 17:52:27 +0900

    issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+
+      wday_num = (@todays_date + x).wday # 曜日を取得
+
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]  # 曜日を追加
+    }
       @week_days.push(days)
     end
 
