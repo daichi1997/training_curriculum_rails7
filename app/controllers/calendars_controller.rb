95168be6d0561b01052677e3370001e70a98d91c
daichi1997 <tagaaan@gmail.com>
2024-08-02 14:51:36 +0900

    issue2のWeekをweekに変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    get_Week #スネークケースに修正
+    get_week #スネークケースに修正
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_Week #スネークケースに修正
+  def get_week #スネークケースに修正
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
